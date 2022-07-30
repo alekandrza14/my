a6fb7ff04f4190a7fbffa714ba87871099052661
unauticna <66287905+alekandrza14@users.noreply.github.com>
2022-07-30 21:57:32 +0500

    0.6

---
+++ MyEngine/MyEngine/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toe;
+
+namespace MyEngine
+{
+    class Class2
+    {
+        void v()
+        {
+
+
+            
+        }
+
+        }
+    }
